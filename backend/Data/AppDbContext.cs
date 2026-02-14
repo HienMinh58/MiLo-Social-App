db7d83558b9f73e25d6fe404fb30b0510a23ecf2
HienMinh58 <nguyenhienminh0508@gmail.com>
2026-02-14 17:00:47 +1300

    Update AppDbContext

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -8,11 +8,32 @@ public class AppDbContext : IdentityDbContext<User>
         : base(options)
     {}
         //Create Tables here
-
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<Like> Likes => Set<Like>();
+        public DbSet<Comment> Comments => Set<Comment>();
+        public DbSet<Friend> Friends => Set<Friend>();
         protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
         //Create Relationships here
+
+        //Post Relationship
+        
+        //Friend Relationship
+        builder.Entity<Friend>()
+            .HasKey(f => new { f.RequesterId, f.AddresseeId});
+        
+        builder.Entity<Friend>()
+            .HasOne(f => f.Requester)
+            .WithMany(u => u.SentRequests)
+            .HasForeignKey(f => f.RequesterId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.Entity<Friend>()
+            .HasOne(f => f.Addressee)
+            .WithMany(u => u.ReceivedRequests)
+            .HasForeignKey(f => f.AddresseeId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
     
 }

c1ba62b117f7bd95522a8e8932c5a243231c7ab3
HienMinh58 <nguyenhienminh0508@gmail.com>
2026-02-24 20:45:26 +1300

    Update User

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -6,6 +6,9 @@ public class User : IdentityUser
     public string? Avatar { get; set; }
 
     //Navigation
+    public ICollection<Post> Posts { get; set; } = new List<Post>();
+    public ICollection<Like> Likes { get; set; } = new List<Like>();
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
     public ICollection<Friend> SentRequests { get; set; } = new List<Friend>();
     public ICollection<Friend> ReceivedRequests { get; set; } = new List<Friend>();
 }

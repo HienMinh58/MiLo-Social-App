8c84c76671449e8f3adacfcc096cd0061c4b759c
HienMinh58 <nguyenhienminh0508@gmail.com>
2026-02-24 20:51:07 +1300

    Create relation User and Post, Post and Likes

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -12,13 +12,32 @@ public class AppDbContext : IdentityDbContext<User>
         public DbSet<Like> Likes => Set<Like>();
         public DbSet<Comment> Comments => Set<Comment>();
         public DbSet<Friend> Friends => Set<Friend>();
+        public DbSet<Message> Messages => Set<Message>();
         protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
         //Create Relationships here
 
         //Post Relationship
-        
+        builder.Entity<Post>()
+            .HasOne(p => p.User)
+            .WithMany(u => u.Posts)
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+            
+        //Like Relationship
+        builder.Entity<Like>()
+            .HasOne(l => l.Post)
+            .WithMany(p => p.Likes)
+            .HasForeignKey(l => l.PostId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Like>()
+            .HasOne(l => l.User)
+            .WithMany(u => u.Likes)
+            .HasForeignKey(l => l.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
         //Friend Relationship
         builder.Entity<Friend>()
             .HasKey(f => new { f.RequesterId, f.AddresseeId});
